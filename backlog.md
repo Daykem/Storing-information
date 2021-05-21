0cb60cd95a0b65d07da74dac3cc3456cb9c2d0cc
tomrouvier <tom.rouvier@epitech.eu>
2021-05-21 03:44:09 +0100

    Edit: backlog

---
+++ backlog.md
@@ -0,0 +1,22 @@
+# Backlog
+
+### 1 : Task estimation (1 hour)
+- Select the language to use
+- Estimate tasks
+- Structure the project
+
+### 2: Creation of the background (2 hour)
+- Create a database via txt file
+- Parse this file
+- Create unit test
+
+### 3: Create method for add students with their grades (1 hours)
+- Create input method for get want you want 
+- Development of the required methods
+- Development of the grade unit tests
+
+### 4: Create method for calculate statistic (1 hour)
+- Create average function
+- Create variance function
+- Create standard deviation function
+- Development of the statistics unit tests
--- src/someproject.py
+++ src/someproject.py
@@ -1,29 +1,37 @@
-
 import math
+import csv
 
+# Function for get the average of a lists   
 def averageOfList(num):
     sumOfNumbers = 0
     for t in num:
         sumOfNumbers = sumOfNumbers + t
-
     avg = sumOfNumbers / len(num)
     return avg
 
-# We relay on our previous implementation for the variance
+# Function for get the variance of a lists   
 def variance(data, ddof=0):
     n = len(data)
     mean = sum(data) / n
     return sum((x - mean) ** 2 for x in data) / (n - ddof)
 
+# Function for get the standard déviation of a lists   
 def stdev(data):
     var = variance(data)
     std_dev = math.sqrt(var)
     return std_dev
 
 
-f = "database.txt"
+# path of database file
+f = "../database.txt"
+
+# Top of the table
 d = {'': ['Design','Implementation','Testing', "Documentation"]}
 
+# Top of the table for csv file
+tabcsv = [['', 'Design','Implementation','Testing', "Documentation"]]
+
+# Read database file and stock into object table variable "d"
 with open(f, "r") as fp:
     line = fp.readline().strip()
     cnt = 0
@@ -34,14 +42,16 @@ with open(f, "r") as fp:
         cnt += 1
         d[a[0] + " " + a[1]] = value
 
+# Print table
 for k, v in d.items():
     lang, perc, change, doc = v
     print ("{:<18} {:<10} {:<15} {:<11} {:<11}".format(k, lang, perc, change, doc))
-    
+
+# Loop for the application
 while (1):
+    quest = input ("\nPress a to add new studentGrade, s to get statistic, csv to export csv file, q to quit the program: \n")
 
-    quest = input ("\nPress a to add new studentGrade, s to get statistic, q to quit the program: \n")
-
+    # For Add a new Student with their Grades
     if (quest == "a"):
         name = input ("Add firstname lastname : ")
         design = input ("Add Design grades : ")
@@ -50,12 +60,14 @@ while (1):
         documentation = input ("Add Documentation grades : ")
         add_note = "\n"+ name + " " + design + " " + implementation + " " + testing + " " + documentation + ""
 
-        f = open("database.txt", "a")
+        # Add on database
+        f = open("../database.txt", "a")
         f.write(add_note)
 
-        f = "database.txt"
+        f = "../database.txt"
         d = {'': ['Design','Implementation','Testing', "Documentation"]}
 
+        # Refresh table
         with open(f, "r") as fp:
             line = fp.readline().strip()
             cnt = 0
@@ -66,14 +78,17 @@ while (1):
                 cnt += 1
                 d[a[0] + " " + a[1]] = value
 
+        # Display Table
         for k, v in d.items():
             lang, perc, change, doc = v
             print ("{:<18} {:<10} {:<15} {:<11} {:<11}".format(k, lang, perc, change, doc))
 
+        # Display statistics
     if (quest == "s"):
-        filename = "database.txt"
+        filename = "../database.txt"
         statistique = {'': ['Average','Standard Deviation','Minimum Grade', "Maximum Grade"]}
 
+        # Calc statistics
         with open(filename, "r") as fp:
             line = fp.readline().strip()
             cnt = 0
@@ -89,9 +104,28 @@ while (1):
                 cnt += 1
                 statistique[a[0] + " " + a[1]] = value
 
+        # Display statistics
         for k, v in statistique.items():
             lang, perc, change, doc = v
             print ("{:<15} {:<9} {:<20} {:<15} {:<11}".format(k, lang, perc, change, doc))
 
+    # Refresh csv file
+    if (quest == "csv"):
+        tabcsv = [['', 'Design','Implementation','Testing', "Documentation"]]
+        with open(f, "r") as fp:
+            line = fp.readline().strip()
+            cnt = 0
+            while line:
+                a = line.split(" ")
+                value = [a[2], a[3], a[4], a[5]]
+                linecsv = [a[0] + " " + a[1], a[2], a[3], a[4], a[5]]
+                tabcsv.append(linecsv)
+                line = fp.readline().strip()
+                cnt += 1
+        with open('list.csv', 'w') as filecsv:
+            write = csv.writer(filecsv)
+            write.writerows(tabcsv)
+
+    # Quit the program
     if (quest == "q"):
         exit(0)
